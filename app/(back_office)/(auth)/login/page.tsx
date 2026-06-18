d1923c1a35441558ede4e8834b618d6f2d01c1d6
benoitmondjo <benoitmondjo@gmail.com>
2026-06-18 17:25:08 +0100

    update login form

--- app/(back_office)/(auth)/login/page.tsx
+++ app/(back_office)/(auth)/login/page.tsx
@@ -1,3 +1,5 @@
+import { MdOutlineMailOutline } from "react-icons/md";
+import { GiPadlock } from "react-icons/gi";
 
 export default function LoginPage()
 {
@@ -5,34 +7,45 @@ export default function LoginPage()
     return (<>
 
         <div className="min-h-screen flex items-center justify-center">
-            <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-md">
+            <div className="bg-white p-8 rounded-xl shadow-lg w-full sm:w-md">
             
             {/* Logo */}
-            <div className="mb-4">
-                <span className="font-holtwood text-2xl text-primary">PORTAIL </span>
-                <span className="font-inclusive text-2xl">Porfolio</span>
+            <div className="mb-10">
+                <div className="mb-3">
+                    <span className="font-holtwood text-2xl text-primary">PORTAIL </span>
+                    <span className="font-inclusive text-2xl">Porfolio</span>
+                </div>
+
+                {/* Sous-titre */}
+                <p className="text-gray-500 mb-6 font-poppins">Veuillez vous connecter</p>
+
             </div>
 
-            {/* Sous-titre */}
-            <p className="text-gray-500 mb-6 font-poppins">Veuillez vous connecter</p>
-
+           
             {/* Champ email */}
-            <input
-                type="email"
-                placeholder="exemple@gmail.fr"
-                className="w-full border-[1.5] border-gray-500 rounded-lg px-4 py-3 mb-4 outline-none focus:border-primary"
-            />
+            <div className="mb-10 rounded-lg border-primary border flex items-center justify-between py-1 px-3 w-full">
+                <input
+                    type="email"
+                    placeholder="exemple@gmail.fr"
+                    className="w-full outline-none border-none text-gray-600 py-2 block"
+                />
+                <MdOutlineMailOutline className="text-2xl text-gray-600" />
+            </div>
+           
 
             {/* Champ mot de passe */}
-            <input
-                type="password"
-                placeholder="*******************"
-                className="w-full border-[1.5] border-gray-500 rounded-lg px-4 py-3 mb-6 outline-none focus:border-primary"
-            />
+            <div className="mb-10 rounded-lg border-primary border flex items-center justify-between py-1 px-3 w-full">
+                <input
+                    type="password"
+                    placeholder="*******************"
+                    className="w-full outline-none border-none text-gray-600 py-2 block"
+                />
+                <GiPadlock className="text-2xl text-gray-600" />
+            </div>
 
             {/* Bouton connexion */}
             <button className="w-full font-poppins bg-primary text-white py-3 rounded-lg hover:opacity-90 transition-opacity duration-300 cursor-pointer">
-             Connexion
+                Connexion
             </button>
 
         </div>
