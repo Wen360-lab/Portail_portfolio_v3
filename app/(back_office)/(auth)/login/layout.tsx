4de2019a6461214e27740142b4d66a2075a6a0b1
benoitmondjo <benoitmondjo@gmail.com>
2026-06-19 01:58:46 +0100

    update login admin form

--- app/(back_office)/(auth)/login/layout.tsx
+++ app/(back_office)/(auth)/login/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 // import "@/public/bg-login.jpeg";
 import "@/style/tailwind.css";
 
-
-
 export const metadata: Metadata = {
   title: "Portail Portfolios | login",
 };
@@ -12,12 +10,7 @@ export default function RootLayout({ children }: Readonly<{children: React.React
 {
     return (
         <html lang="fr" >
-            <body   className="min-h-screen flex items-center justify-center"
-            style={{ 
-                backgroundImage: "url('/bg-login.jpeg')", 
-                backgroundSize: "cover", 
-                backgroundPosition: "center" 
-            }}>
+            <body   className="h-screen flex items-center justify-center bg-login-admin-form">
                 {children}
             </body>
         </html>
