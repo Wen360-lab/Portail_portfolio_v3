711b87c2b94609c62d1fbad7dda921a5004366f6
benoitmondjo <benoitmondjo@gmail.com>
2026-06-22 08:50:14 +0100

    updata data

--- lib/mock/data.ts
+++ lib/mock/data.ts
@@ -1,3 +1,4 @@
+import { fieldsRest } from "@hookform/resolvers/ajv/src/__tests__/__fixtures__/data-errors.js";
 
 /**
  * Le type qui definie un profile
@@ -108,42 +109,42 @@ const profiles: ArrayProfilesType = [
         photo: "/profile/photos/profile-jean-junior.png",
     },
 
-    // {
-    //     initials: "AUR",
-    //     name: "Asseekome Ulrich Romaric",
-    //     role: "Développeur web",
-    //     desc: "Ulrich, licence pro en informatique de gestion, developpeur web et web mobile junior (Front-end), certifié D-clic. Promo 14 école 241.",
-    //     tags: ["HTML", "CSS", "Node.js"],
-    //     link: "https://asseekomeulrichromaric-lang.github.io/Portfolio/",
-    //     photo: "/profile/photos/profile-ulrich.jpg",
-    // },
-    // {
-    //     initials: "AMV",
-    //     name: "Vanelie ABANG MINKO",
-    //     role: "Développeur web",
-    //     desc: "Je m'appelle Vanelie ABANG MINKO, nouvelle dans le domaine du développement web et passionnée par les technologies numériques. Je suis développeuse web back-end et infographe. Curieuse et motivée, j'aime apprendre de nouvelles technologies, relever des défis et améliorer mes compétences à travers différents projets web. Cette aventure dans le développement me permet de découvrir chaque jour de nouvelles connaissances et de progresser continuellement.",
-    //     tags: ["HTML", "CSS", "Node.js"],
-    //     link: "https://akatsuki-png.github.io/MonPortfolio2.0/",
-    //     photo: "/profile/photos/profile-vanelie.png",
-    // },
-    // {
-    //     initials: "MDB",
-    //     name: "MOUNDOUNGA DIBANGOU Beeve",
-    //     role: "Front-end",
-    //     desc: "Développeur attiré par le code, designer graphique, penchant pour l'esthétique visuel, crée des interfaces modernes et professionnels.",
-    //     tags: ["HTML", "CSS", "Node.js"],
-    //     link: "https://wwwyurysherman777-glitch.github.io/portfolio/",
-    //     photo: "/profile/photos/profile-beev.jpg",
-    // },
-    // {
-    //     initials: "MBO",
-    //     name: "MBA Osuris",
-    //     role: "Développeur Web",
-    //     desc: "Passionné par la création numérique, mon portfolio illustre mes compétences, qui vont de l'UI/UX Design (Zoning, Wireframe, Maquettage sur des projets comme PrimePrompts) jusqu'à l'intégration front-end moderne (HTML5, CSS3, JavaScript) pour des sites fluides et responsives.",
-    //     tags: ["HTML", "CSS", "JS"],
-    //     link: "https://bekerosuris-tech.github.io/portfolio/",
-    //     photo: "/profile/photos/profile-osirus.png",
-    // },
+    {
+        id: 10,
+        lastname: "Asseekome",
+        firstname: "Ulrich Romaric",
+        job: jobs[0],
+        desc: "Ulrich, licence pro en informatique de gestion, developpeur web et web mobile junior (Front-end), certifié D-clic. Promo 14 école 241.",
+        portfolio: "https://asseekomeulrichromaric-lang.github.io/Portfolio/",
+        photo: "/profile/photos/profile-ulrich.jpg",
+    },
+    {
+        id: 11,
+        lastname: "ABANG MINKO",
+        firstname: "Vanelie",
+        job: jobs[0],
+        desc: "Je m'appelle Vanelie ABANG MINKO, nouvelle dans le domaine du développement web et passionnée par les technologies numériques. Je suis développeuse web back-end et infographe. Curieuse et motivée, j'aime apprendre de nouvelles technologies, relever des défis et améliorer mes compétences à travers différents projets web. Cette aventure dans le développement me permet de découvrir chaque jour de nouvelles connaissances et de progresser continuellement.",
+        portfolio: "https://akatsuki-png.github.io/MonPortfolio2.0/",
+        photo: "/profile/photos/profile-vanelie.png",
+    },
+    {
+        id: 12,
+        lastname: "MOUNDOUNGA DIBANGOU ",
+        firstname: "Beeve",
+        job: jobs[0],
+        desc: "Développeur attiré par le code, designer graphique, penchant pour l'esthétique visuel, crée des interfaces modernes et professionnels.",
+        portfolio: "https://wwwyurysherman777-glitch.github.io/portfolio/",
+        photo: "/profile/photos/profile-beev.jpg",
+    },
+    {
+        id:13,
+        lastname: "MBA",
+        firstname: "Osuris",
+        job: jobs[0],
+        desc: "Passionné par la création numérique, mon portfolio illustre mes compétences, qui vont de l'UI/UX Design (Zoning, Wireframe, Maquettage sur des projets comme PrimePrompts) jusqu'à l'intégration front-end moderne (HTML5, CSS3, JavaScript) pour des sites fluides et responsives.",
+        portfolio: "https://bekerosuris-tech.github.io/portfolio/",
+        photo: "/profile/photos/profile-osirus.png",
+    },
     // {
     //     initials: "MK",
     //     name: "Konda ndoundou rucia berlina",
